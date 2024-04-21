5089ce7ef1e04d2dcb6cdb578689ff9ee8a93c7f
Xavi Domínguez <xavi@fablabbcn.org>
2024-04-21 20:04:31 +0200

    Update ParlantAmb.py

--- ParlantAmb.py
+++ ParlantAmb.py
@@ -31,7 +31,7 @@ if "disabled" not in st.session_state:
 
 with st.sidebar.form("usuari_form"):
   nom = st.text_input("Escriu la teva identificacio 👇",disabled=st.session_state.disabled)
-  submit_button = st.form_submit_button(label="Iniciar Xat",disabled=st.session_state.disabled)
+  submit_button = st.form_submit_button(label="Iniciar Xat",disabled=st.session_state.disabled,on_click=disable)
 
 
   if submit_button and nom != '' and nom in l1:
