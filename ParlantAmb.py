1fc42b504973d1cf696c7790ce3438789a99350c
Xavi Domínguez <xavi@fablabbcn.org>
2024-04-21 21:58:38 +0200

    Update ParlantAmb.py

--- ParlantAmb.py
+++ ParlantAmb.py
@@ -50,13 +50,12 @@ with st.sidebar.form("usuari_form"):
   nom = st.text_input("Escriu la teva identificacio 👇",disabled=st.session_state.disabled, key=1)
   submit_button = st.form_submit_button(label="Iniciar Xat",disabled=st.session_state.disabled, on_click=disable)
 
-  if st.sidebar.button("Iniciar Xat"):
-    if nom != '' and nom in l1:
+  if submit_button and nom != '' and nom in l1:
         st.session_state.start_chat = True
         st.session_state.disabled = True
         thread = client.beta.threads.create()
         st.session_state.thread_id = thread.id
-    else:
+  else:
         if nom != '':
             st.sidebar.write(":red[Aquest usuari no existeix]")
 
