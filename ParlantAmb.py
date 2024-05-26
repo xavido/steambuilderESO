2e5be1901d85994955ec98672004eee01a04ddeb
Xavi Domínguez <xavi@fablabbcn.org>
2024-05-26 21:51:34 +0200

    Update ParlantAmb.py

--- ParlantAmb.py
+++ ParlantAmb.py
@@ -164,8 +164,7 @@ if st.session_state.start_chat:
             st.session_state.messages.append({"role": "assistant", "content": message.content[0].text.value})
             with st.chat_message("assistant"):
                 st.markdown(message.content[0].text.value)
-                if tiene_falta_ortografia(prompt):
-                    st.markdown('Per cert... la teva consulta sembla que té alguna errada ortogràfica')
+                
 
 # Crea una conexión con la base de datos
         conn = mysql.connector.connect(host=db_host, port=db_port, database=db_name, user=db_user,
