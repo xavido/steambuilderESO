c5453e706c550a6b38eeb919709e9bf9fe9d1fac
Xavi Domínguez <xavi@fablabbcn.org>
2024-05-26 22:04:24 +0200

    Update Report.py

--- Report.py
+++ Report.py
@@ -169,6 +169,9 @@ if st.session_state.start_chat:
         # create two columns for charts
         num_preguntas_cortas = sum(1 for pregunta in df["pregunta"] if len(pregunta.split()) <= 2)
         # Contar el número de preguntas con al menos una falta de ortografía
+        preguntas = df['pregunta'].tolist()
+        preguntas_unicas = set(preguntas)
+        num_preguntas_repetidas = len(preguntas) - len(preguntas_unicas)
 
         fig_colA, fig_colB = st.columns(2)
         with fig_colA:
@@ -177,7 +180,7 @@ if st.session_state.start_chat:
             st.markdown("##### # de consultes NO vàlides (<2 paraules incloent salutacions):**"+str(num_preguntas_cortas)+"**")
             
         with fig_colB:
-            st.markdown("##### # de consultes amb faltes ortografia")
+            st.markdown("##### # de consultes repetides:"+str(num_preguntas_repetidas))
 
         fig_col1, fig_col2 = st.columns(2)
 
