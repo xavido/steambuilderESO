813ed58c86986ff8a1a79fceb1655acec41eec04
Xavi Domínguez <xavi@fablabbcn.org>
2024-11-26 12:47:03 +0100

    Update ParlantAmb.py

--- ParlantAmb.py
+++ ParlantAmb.py
@@ -31,7 +31,7 @@ st.set_page_config(page_title="IE Rafael Alberti - Parlant amb la Júlia... un x
 
 openai.api_key = st.secrets["auto_pau"]
 
-l1 = ['xdominguez', 'aorti', 'aabdelradi','hboutafah','mdabtyan']
+l1 = ['xdominguez','aorti', 'aabdelradi','hboutafah','mdabtyan']
 l2 = ['mahmed','laranibar','mcuadra','omghari','dghita','mlakhsayssi','wlaroussi','rmartinez','jmendoza','smoujar','jrivas','dsierra','jtinoco','xwang','wyemlahi']
 l3 = ['malarbi']
 l4 = ['zali','hmerroun']
@@ -65,7 +65,7 @@ def autoplay_audio(file_path: str):
         )
 
 def disable():
-    if nom != '' and nom in l1:
+    if nom != '' and nom in l1 or nom in l2 or nom in l3 or nom in l4 or nom in l5 or nom in l6 or nom in l7:
         st.session_state.disabled = True
         st.session_state.start_chat = True
         st.session_state.disabled = True
